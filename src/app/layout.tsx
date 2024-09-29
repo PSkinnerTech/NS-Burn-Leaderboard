bee4e1ce9b400f27fd578b60c012d36d91c42602
PSkinnerTech <patrick@clgcorporation.com>
2024-09-29 16:49:33 +0800

    added shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
 import { useMemo } from 'react';
+import '@/app/globals.css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const network = WalletAdapterNetwork.Mainnet;
