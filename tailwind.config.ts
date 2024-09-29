ce2c371d08294011172d54b97c115245e154efae
PSkinnerTech <patrick@clgcorporation.com>
2024-09-29 17:42:03 +0800

    added pulse animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+		animation: {
+			fade: 'fadeIn .5s ease-in-out',
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
